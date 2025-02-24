dd7ab4fd5e72708264f2170c311a7a353da83ef0
Garrett59100 <garrett59100@gmail.com>
2025-02-23 23:54:32 -0500

    actually fixed mbb this time

--- main_body_board/src/main.cpp
+++ main_body_board/src/main.cpp
@@ -1,3 +1,5 @@
+#include <memory>
+
 #include "../include/Constants.h"
 #include "../include/DEBUG.h"
 
@@ -21,48 +23,51 @@
 QuadratureDecoder demo_encoder{ENC_A_PIN_0, ENC_B_PIN_0};
 #endif
 
-unsigned long currentRunCycle = 0;
-
 // create the main body board
-MainBodyBoard mbb {&currentRunCycle};
+unsigned long currentRunCycle = 0;
+std::shared_ptr<MainBodyBoard> mbb;
 
 void setup()
 {
-  // start up the main body board, this will turn the status light off
-  mbb.startUp();
+	// *mbb = MainBodyBoard{&currentRunCycle};
+	mbb = std::make_shared<MainBodyBoard>(&currentRunCycle);
+	//   start up the main body board, this will turn the status light off
+	Serial.println("before startup");
+	mbb->startUp();
+	Serial.println("finished startup");
 
-  #if ENABLE_SERIAL
-  //this is the connection to the computer
-  Serial.begin(9600);
-  Serial.println("Main Body Board");
-  #endif
+#if ENABLE_SERIAL
+	// this is the connection to the computer
+	Serial.begin(9600);
+	Serial.println("Main Body Board");
+#endif
 
-  #if ENABLE_DEMO_ENCODER
-  demo_encoder.begin();
-  #endif
+#if ENABLE_DEMO_ENCODER
+	demo_encoder.begin();
+#endif
 
-  //update the currentRunCycle to be synced with the current time
-  currentRunCycle = floor(millis()/UPDATE_RATE_MS);
+	// update the currentRunCycle to be synced with the current time
+	currentRunCycle = floor(millis() / UPDATE_RATE_MS);
 }
 
 void loop()
 {
-  mbb.runBackgroundProcess();
-  if(millis() >= UPDATE_RATE_MS * currentRunCycle)
-  {
-    #if ENABLE_SERIAL
-      Serial.print("current cycle: ");
-      Serial.println((int)currentRunCycle);
+	mbb->runBackgroundProcess();
+	if (millis() >= UPDATE_RATE_MS * currentRunCycle)
+	{
+#if ENABLE_SERIAL
+		Serial.print("current cycle: ");
+		Serial.println((int)currentRunCycle);
 
-      #if ENABLE_DEMO_ENCODER
-      Serial.println(demo_encoder.getRPM(millis() - UPDATE_RATE_MS * (currentRunCycle-1)));
-      #endif
-    #endif // ENABLE_SERIAL
-    
-    // subtract the current cycle by one to get the entire timeframe
-    mbb.updateSubsystems(millis() - UPDATE_RATE_MS * (currentRunCycle-1));
+#if ENABLE_DEMO_ENCODER
+		Serial.println(demo_encoder.getRPM(millis() - UPDATE_RATE_MS * (currentRunCycle - 1)));
+#endif
+#endif // ENABLE_SERIAL
 
-    // the increment to the next cycle
-    currentRunCycle++;
-  }
+		// subtract the current cycle by one to get the entire timeframe
+		mbb->updateSubsystems(millis() - UPDATE_RATE_MS * (currentRunCycle - 1));
+
+		// the increment to the next cycle
+		currentRunCycle++;
+	}
 }
