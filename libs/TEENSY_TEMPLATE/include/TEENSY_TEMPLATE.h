ec401efb28c52715f7043ee4ba06ee7f8ebebcc5
Lucas Newcomb <pantsbabyy@gmail.com>
2025-09-17 10:42:39 -0400

    Created basic teensy template library

---
+++ libs/TEENSY_TEMPLATE/include/TEENSY_TEMPLATE.h
@@ -0,0 +1,14 @@
+#ifndef TEENSY_TEMPLATE_H
+#define TEENSY_TEMPLATE_H
+
+class TeensyTemplate {
+protected:
+    bool m_disabled = false;
+public:
+    virtual void startUp() = 0;
+    virtual void blinkStatusLight() = 0;
+    virtual void disable() = 0;
+    bool isDisabled();
+};
+
+#endif
---
+++ libs/TEENSY_TEMPLATE/src/TEENSY_TEMPLATE.cpp
@@ -0,0 +1,5 @@
+#include "../include/TEENSY_TEMPLATE.h"
+
+bool TeensyTemplate::isDisabled() {
+	return m_disabled;
+}
