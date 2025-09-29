9e2def4bded6a1002f71876f037a39a73797ccb9
pants721 <lucas@nukem.dev>
2025-09-29 17:32:35 -0400

    Updated setup and format scripts to work with Ubuntu

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,13 +5,22 @@ git submodule update --init --recursive
 echo "Installing clang-tidy and clang-format"
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+
+sudo add-apt-repository 'deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic main'
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+sudo apt update
+
 echo "Installing clang-format using apt"
-sudo apt install clang-format-19
-echo "alias clang-format='clang-format-19'" >> ~/.bashrc
+sudo apt install clang-format-20
+echo "alias clang-format='clang-format-20'" >> ${USER_HOME}/.bashrc
 
 echo "Installing clang-tidy using apt"
-sudo apt install clang-tidy-19
-echo "alias clang-tidy='clang-tidy-19'" >> ~/.bashrc
+sudo apt install clang-tidy-20
+echo "alias clang-tidy='clang-tidy-20'" >> ${USER_HOME}/.bashrc
 
 echo "Installing platformio core"
 sudo apt install platformio
+
+echo "Installing dos2unix"
+sudo apt install dos2unix
