318aa8f36182661a245335a27f7eed155424a9c3
Lucas Newcomb <pantsbabyy@gmail.com>
2026-01-13 12:12:04 -0500

    Updated CAN message IDs to align with interface document

--- libs/CAN/include/CAN/message_id.h
+++ libs/CAN/include/CAN/message_id.h
@@ -16,25 +16,25 @@
 enum class MessageID : uint32_t
 {
     E_STOP = (uint32_t)0,
+    ROS_HEARTBEAT = (uint32_t)1,
 
     // arm
-    ENABLE_ARM = (uint32_t)10,
-    MOVE_BASE = (uint32_t)11,
-    MOVE_SHOULDER = (uint32_t)12,
-    MOVE_ELBOW = (uint32_t)13,
-    BEND_WRIST = (uint32_t)14,
-    TWIST_WRIST = (uint32_t)15,
-    MOVE_CLAW = (uint32_t)16,
-    MOVE_SOLENOID = (uint32_t)17,
+    ENABLE_ARM = (uint32_t)20,
+    MOVE_BASE = (uint32_t)21,
+    MOVE_SHOULDER = (uint32_t)22,
+    MOVE_ELBOW = (uint32_t)23,
+    BEND_WRIST = (uint32_t)24,
+    TWIST_WRIST = (uint32_t)25,
+    MOVE_CLAW = (uint32_t)26,
+    MOVE_SOLENOID = (uint32_t)27,
+    BASE_POS = (uint32_t)28,
 
     // science
-    ENABLE_SCIENCE = (uint32_t)20,
-    MOVE_AUGER = (uint32_t)21,
-    HOME_AUGER = (uint32_t)22,
-    ENABLE_DRILL = (uint32_t)23,
-    MOVE_SLIDE = (uint32_t)24,
-
-    HEARTBEAT,
+    ENABLE_SCIENCE = (uint32_t)30,
+    MOVE_AUGER = (uint32_t)31,
+    HOME_AUGER = (uint32_t)32,
+    ENABLE_DRILL = (uint32_t)33,
+    MOVE_SLIDE = (uint32_t)34,
 };
 
 #endif // SPEX_CAN_MESSAGE_ID_H
