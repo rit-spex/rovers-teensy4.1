1cd6fc2b54e29294b1d28db0086cbe59876d1d97
Lucas Newcomb <pantsbabyy@gmail.com>
2026-02-23 12:16:44 -0500

    Updated message ID's

--- libs/CAN/include/CAN/message_id.h
+++ libs/CAN/include/CAN/message_id.h
@@ -21,7 +21,6 @@ enum class MessageID : uint32_t
 
     // chassis
     ENABLE_CHASSIS = (uint32_t)10,
-    TARGET_RPM = (uint32_t)11,
     DRIVE_POWER = (uint32_t)13,
 
     // arm
@@ -33,14 +32,31 @@ enum class MessageID : uint32_t
     TWIST_WRIST = (uint32_t)25,
     MOVE_CLAW = (uint32_t)26,
     MOVE_SOLENOID = (uint32_t)27,
-    BASE_POS = (uint32_t)28,
+
+    READ_WRIST_BEND = (uint32_t)33,
+    READ_WRIST_TWIST = (uint32_t)34,
+    READ_CLAW = (uint32_t)35,
 
     // science
     ENABLE_SCIENCE = (uint32_t)40,
     MOVE_AUGER = (uint32_t)41,
-    HOME_AUGER = (uint32_t)42,
-    ENABLE_DRILL = (uint32_t)43,
-    MOVE_SLIDE = (uint32_t)44,
+    ENABLE_DRILL = (uint32_t)42,
+    MOVE_SLIDE = (uint32_t)43,
+    ENABLE_PUMP = (uint32_t)44,
+    MOVE_SPECTROMETER_SLIDE = (uint32_t)48,
+    MOVE_FLUOROMETER_SLIDE = (uint32_t)49,
+    ENABLE_FLUOROMETER_MICRO_PUMP = (uint32_t)50,
+    ENABLE_PRIMER = (uint32_t)51,
+    ENABLE_VIBRATION = (uint32_t)52,
+
+    READ_AUGER = (uint32_t)60,
+    READ_SLIDE = (uint32_t)61,
+    READ_DRILL = (uint32_t)62,
+    READ_PUMPS = (uint32_t)63,
+    READ_SPECTROMETER_SLIDE = (uint32_t)64,
+    READ_FLUOROMETER_SLIDE = (uint32_t)65,
+    READ_SPECTROMETER_LIGHT = (uint32_t)66,
+    READ_FLUOROMETER_COLOR_SENSOR = (uint32_t)67,
 };
 
 #endif // SPEX_CAN_MESSAGE_ID_H
